00b9a2ebc8c172619d7122cb08fa80bf4ea91658
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 21:18:35 +0600

    Update __init__.py

--- Powers/database/sql/__init__.py
+++ Powers/database/sql/__init__.py
@@ -2,15 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from Powers import SQLURL as DB_URI
+from Powers import SQLURL
 from Powers import LOGGER as log
 
-if DB_URI and DB_URI.startswith("postgres://"):
-    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
+if SQLURL and SQLURL.startswith("postgres://"):
+    SQLURL = SQLURL.replace("postgres://", "postgresql://", 1)
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    engine = create_engine(SQLURL, client_encoding="utf8")
     log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
