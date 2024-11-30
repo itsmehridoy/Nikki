5c952d5c19d3e4e8a88d782ca209a8005912583e
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 17:44:59 +0600

    Update misc.py

--- Powers/misc.py
+++ Powers/misc.py
@@ -57,3 +57,16 @@ async def cache_support():
     SUPPORT_USERS["Dev"] = SUPPORT_USERS["Dev"].union(devs)
     SUPPORT_USERS["Sudo"] = SUPPORT_USERS["Sudo"].union(sudo)
     return
+###########
+from asyncio import gather
+from httpx import AsyncClient, Timeout
+
+# HTTPx Async Client
+fetch = AsyncClient(
+    verify=False,
+    headers={
+        "Accept-Language": "en-US,en;q=0.9,id-ID;q=0.8,id;q=0.7",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.42",
+    },
+    timeout=Timeout(20),
+)
