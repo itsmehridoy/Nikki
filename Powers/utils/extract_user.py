a4bcbc5caef328b09a300e9732c9b5e173c4112f
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 14:17:19 +0600

    Update extract_user.py

--- Powers/utils/extract_user.py
+++ Powers/utils/extract_user.py
@@ -20,7 +20,7 @@ async def extract_user(c: Nikki, m: Message) -> Tuple[int, str, str]:
         user_first_name = m.reply_to_message.from_user.first_name
         user_name = m.reply_to_message.from_user.username
 
-    elif len(m.text.split()) > 1:
+    elif len(m.command) > 1:
         if len(m.entities) > 1:
             required_entity = m.entities[1]
             if required_entity.type == entity.TEXT_MENTION:
@@ -30,14 +30,14 @@ async def extract_user(c: Nikki, m: Message) -> Tuple[int, str, str]:
             elif required_entity.type in (entity.MENTION, entity.PHONE_NUMBER):
                 # new long user ids are identified as phone_number
                 user_found = m.text[
-                    required_entity.offset : (
-                        required_entity.offset + required_entity.length
-                    )
-                ]
+                             required_entity.offset: (
+                                     required_entity.offset + required_entity.length
+                             )
+                             ]
 
                 try:
                     user_found = int(user_found)
-                except (ValueError, Exception) as ef:
+                except Exception as ef:
                     if "invalid literal for int() with base 10:" in str(ef):
                         user_found = str(user_found)
                     else:
@@ -72,7 +72,7 @@ async def extract_user(c: Nikki, m: Message) -> Tuple[int, str, str]:
         else:
             try:
                 user_id = int(m.text.split()[1])
-            except (ValueError, Exception) as ef:
+            except Exception as ef:
                 if "invalid literal for int() with base 10:" in str(ef):
                     user_id = (
                         str(m.text.split()[1])
@@ -98,6 +98,7 @@ async def extract_user(c: Nikki, m: Message) -> Tuple[int, str, str]:
                             user = await c.get_users(user_r.user_id)
                         except Exception as ef:
                             return await m.reply_text(f"User not found ! Error: {ef}")
+                    user_id = user.id
                     user_first_name = user.first_name
                     user_name = user.username
                     LOGGER.error(ef)
