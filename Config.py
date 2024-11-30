ae5fafdd8e507084ec88687170e0e2a96d74cd19
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 14:11:56 +0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,15 +2,15 @@ import os
 
 class Config:
     LOGGER = True
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6517327157:AAHXp2a9g23DqBWWNHaBT5mJbAyjua6XU4g")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8109009407:AAF2XlACddiQH3HWBl4oIVthPKrHdv45ERU")
     API_ID = int(os.environ.get("API_ID", 28412950))
     API_HASH = os.environ.get("API_HASH", "0bfda840b5f2ea2f4053ba6d8813d351")
     OWNER_ID = int(os.environ.get("OWNER_ID", 6213690669))
-    MESSAGE_DUMP = int(os.environ.get("MESSAGE_DUMP", -1001911266279))
+    MESSAGE_DUMP = int(os.environ.get("MESSAGE_DUMP", -1001804048226))
     DEV_USERS = os.environ.get("DEV_USERS", "6213690669")
     SUDO_USERS = os.environ.get("SUDO_USERS", "6213690669")
     WHITELIST_USERS = os.environ.get("WHITELIST_USERS", "6213690669")
-    DB_URI = os.environ.get("DB_URI", "mongodb+srv://Saifs:Robo@cluster0.iug5vxj.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DB_URI", "mongodb+srv://GroupSentry:jT6MpjXATacUb2aU@cluster0.4nzkt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "NikkiDB")
     NO_LOAD = os.environ.get("NO_LOAD", "")
     PREFIX_HANDLER = os.environ.get("PREFIX_HANDLER", "/ !")
