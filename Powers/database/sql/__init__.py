66ae042fa903e5ec1cb3568de7a528f6cd002592
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-11-30 21:30:58 +0600

    Update __init__.py

--- Powers/database/sql/__init__.py
+++ Powers/database/sql/__init__.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from Powers import LOGGER as log
 
+DB_URI = "postgres://u3cf4evmcicphe:p62d2c021e1ff71dd5d335e57a84db7a2f0ef6506647a8cfd34594a91948a3d44@c7u1tn6bvvsodf.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d9k6e1julog4bj"
+
+
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
-
-DB_URI = ""
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
