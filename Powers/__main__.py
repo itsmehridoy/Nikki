ab0651ce211483a47ce19ba7ddfd95a2f806d215
ʜʀɪᴅᴏʏ <itsmehhridoy@gmail.com>
2024-11-30 21:11:42 +0600

    Update __main__.py

--- Powers/__main__.py
+++ Powers/__main__.py
@@ -1,7 +1,7 @@
-from Config import BOT_TOKEN
+from Config import Config
 from Powers import telethn
 from Powers.bot_class import Nikki
 
 if __name__ == "__main__":
-    telethn.start(bot_token=BOT_TOKEN)
+    telethn.start(bot_token=Config.BOT_TOKEN)
     Nikki.run()
