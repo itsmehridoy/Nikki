1a038e30f1eaaec0b3b6d890366a3e2ac092de6f
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-12-05 06:13:15 +0600

    Code Clean up!

--- Powers/events.py
+++ Powers/events.py
@@ -1,19 +1,13 @@
 from telethon import events
-
 from Powers import telethn
-
 
 def register(**args):
     """ʀᴇɢɪsᴛᴇʀs ᴀ ɴᴇᴡ ᴍᴇssᴀɢᴇ."""
     pattern = args.get("pattern")
-
     r_pattern = r"^[/!]"
-
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = f"(?i){pattern}"
-
     args["pattern"] = pattern.replace("^/", r_pattern, 1)
-
     def decorator(func):
         telethn.add_event_handler(func, events.NewMessage(**args))
         return func
