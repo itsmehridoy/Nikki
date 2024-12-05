772c5864b6e23c2ed0fd3d72ecdb5697c1d9a052
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-12-05 06:35:42 +0600

    Update disable.py

--- Powers/plugins/disable.py
+++ Powers/plugins/disable.py
@@ -5,14 +5,14 @@ from pyrogram.enums import ChatMemberStatus as CMS
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
-from Powers import HELP_COMMANDS, LOGGER
+from Powers import HELP_COMMANDS
 from Powers.bot_class import Nikki
 from Powers.database.disable_db import Disabling
-from Powers.utils.custom_filters import (admin_filter, can_change_filter,
                                          command, owner_filter)
 
 
-@Nikki.on_message(command("disable") & can_change_filter)
+@Nikki.on_cmd("disable", group_only=True, self_admin=True)
+@Nikki.adminsOnly(permissions="can_change_info", is_both=True)
 async def disableit(_, m: Message):
     if len(m.text.split()) < 2:
         return await m.reply_text("What to disable?")
@@ -24,7 +24,6 @@ async def disableit(_, m: Message):
 
     db = Disabling(m.chat.id)
     disable_list = db.get_disabled()
-    LOGGER.info(f"{m.from_user.id} used disabled cmd in {m.chat.id}")
 
     if str(m.text.split(None, 1)[1]) in disable_list:
         return await m.reply_text("It's already disabled!")
@@ -37,18 +36,14 @@ async def disableit(_, m: Message):
     return
 
 
-@Nikki.on_message(command("disabledel") & can_change_filter)
+@Nikki.on_cmd("disabledel", group_only=True, self_admin=True)
+@Nikki.adminsOnly(permissions="can_change_info", is_both=True)
 async def set_dsbl_action(_, m: Message):
     db = Disabling(m.chat.id)
 
     status = db.get_action()
-    if status == "none":
-        cur = False
-    else:
-        cur = True
+    cur = status != "none"
     args = m.text.split(" ", 1)
-
-    LOGGER.info(f"{m.from_user.id} disabledel used in {m.chat.id}")
 
     if len(args) >= 2:
         if args[1].lower() == "on":
@@ -64,7 +59,8 @@ async def set_dsbl_action(_, m: Message):
     return
 
 
-@Nikki.on_message(command("enable") & can_change_filter)
+@Nikki.on_cmd("enable", group_only=True, self_admin=True)
+@Nikki.adminsOnly(permissions="can_change_info", is_both=True)
 async def enableit(_, m: Message):
     if len(m.text.split()) < 2:
         return await m.reply_text("What to enable?")
@@ -73,37 +69,39 @@ async def enableit(_, m: Message):
     if str(m.text.split(None, 1)[1]) not in disable_list:
         return await m.reply_text("It's not disabled!")
     db.remove_disabled((str(m.text.split(None, 1)[1])).lower())
-    LOGGER.info(f"{m.from_user.id} enabled something in {m.chat.id}")
     return await m.reply_text(f"Enabled {m.text.split(None, 1)[1]}!")
 
 
-@Nikki.on_message(command("disableable") & can_change_filter)
+@Nikki.on_cmd("disableable", group_only=True, self_admin=True)
+@Nikki.adminsOnly(permissions="can_change_info", is_both=True)
 async def disabling(_, m: Message):
     disable_cmd_keys = sorted(
         k
         for j in [HELP_COMMANDS[i]["disablable"] for i in list(HELP_COMMANDS.keys())]
         for k in j
     )
-    tes = "List of commnds that can be disabled:\n"
-    tes += "\n".join(f" • <code>{escape(i)}</code>" for i in disable_cmd_keys)
-    LOGGER.info(f"{m.from_user.id} checked disableable {m.chat.id}")
+    tes = "List of commnds that can be disabled:\n" + "\n".join(
+        f" • <code>{escape(i)}</code>" for i in disable_cmd_keys
+    )
     return await m.reply_text(tes)
 
 
-@Nikki.on_message(command("disabled") & admin_filter)
+@Nikki.on_cmd("disabled", group_only=True, self_admin=True)
+@Nikki.adminsOnly(permissions="can_change_info", is_both=True)
 async def disabled(_, m: Message):
     db = Disabling(m.chat.id)
     disable_list = db.get_disabled()
     if not disable_list:
         await m.reply_text("No disabled items!")
         return
-    tex = "Disabled commands:\n"
-    tex += "\n".join(f" • <code>{escape(i)}</code>" for i in disable_list)
-    LOGGER.info(f"{m.from_user.id} checked disabled {m.chat.id}")
+    tex = "Disabled commands:\n" + "\n".join(
+        f" • <code>{escape(i)}</code>" for i in disable_list
+    )
     return await m.reply_text(tex)
 
 
-@Nikki.on_message(command("enableall") & owner_filter)
+@Nikki.on_cmd("enableall", group_only=True, self_admin=True)
+@Nikki.adminsOnly(only_owner=True)
 async def rm_alldisbl(_, m: Message):
     db = Disabling(m.chat.id)
     all_dsbl = db.get_disabled()
@@ -127,7 +125,7 @@ async def rm_alldisbl(_, m: Message):
     return
 
 
-@Nikki.on_callback_query(filters.regex("^enableallcmds$"))
+@app.on_cb("enableallcmds")
 async def enablealll(_, q: CallbackQuery):
     user_id = q.from_user.id
     user_status = (await q.message.chat.get_member(user_id)).status
@@ -145,7 +143,6 @@ async def enablealll(_, q: CallbackQuery):
         return
     db = Disabling(q.message.chat.id)
     db.rm_all_disabled()
-    LOGGER.info(f"{user_id} enabled all in {q.message.chat.id}")
     await q.message.edit_text("Enabled all!", show_alert=True)
     return
 
