6a1d678e07d62a7cd7949f2a86f45c3230be7d31
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-12-15 22:18:57 +0600

    Update bot_class.py

--- Powers/bot_class.py
+++ Powers/bot_class.py
@@ -1,15 +1,13 @@
 from platform import python_version
 from threading import RLock
-
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from Abg import patch
-
-from Powers import (LOGGER, MESSAGE_DUMP,
-                    NO_LOAD, load_cmds)
+from Powers import (LOGGER, MESSAGE_DUMP, NO_LOAD, load_cmds)
 from Powers.database import MongoDB
 from Powers.plugins import all_plugins
 from Config import Config
+
+from Abg import patch
 
 INITIAL_LOCK = RLock()
 
@@ -23,34 +21,27 @@ class Anony(Client):
             api_hash=Config.API_HASH,
             workers=Config.WORKERS,
         )
-      
+
     async def start(self):
         await super().start()
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
-      
-        startmsg = await self.send_message(MESSAGE_DUMP, "<i>Starting Bot...</i>")
-        LOGGER.info(
-            f"Pyrogram v{__version__} (Layer - {layer}) started on {self.username}",
-        )
-        cmd_list = await load_cmds(await all_plugins())
 
+        startmsg = await self.send_message(MESSAGE_DUMP, "<i>Starting Bot...</i>")
+        LOGGER.info(f"Pyrogram v{__version__} (Layer - {layer}) started on {self.username}")
+        cmd_list = await load_cmds(await all_plugins())
         LOGGER.info(f"Plugins Loaded: {cmd_list}")
         await startmsg.edit_text(
-            (
-                f"<b><i>@{self.username} started on Pyrogram v{__version__} (Layer - {layer})</i></b>\n"
-                f"\n<b>Python:</b> <u>{python_version()}</u>\n"
-                "\n<b>Loaded Plugins:</b>\n"
-                f"<i>{cmd_list}</i>\n"
-            ),
+            f"<b><i>@{self.username} started on Pyrogram v{__version__} (Layer - {layer})</i></b>\n"
+            f"\n<b>Python:</b> <u>{python_version()}</u>\n"
+            f"\n<b>Loaded Plugins:</b>\n<i>{cmd_list}</i>\n"
         )
         LOGGER.info("Bot Started Successfully!\n")
-
     async def stop(self):
         await super().stop()
         MongoDB.close()
         LOGGER.info("Bot Stopped.")
-      
+
 Nikki = Anony()
