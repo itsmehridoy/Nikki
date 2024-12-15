241389e72093eb78f41118883bc2849be5fd2459
˹ʟʏғᴏɪ˼ シ <hridoy169.bd@gmail.com>
2024-12-15 21:43:35 +0600

    Update dev.py

--- Powers/plugins/dev.py
+++ Powers/plugins/dev.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from io import BytesIO
 from time import gmtime, time
 from traceback import format_exc
+from base64 import b64decode
+from inspect import getfullargspec
 from pyrogram.filters import command
 from pyrogram.types import Message
 from pyrogram.errors import (
@@ -31,8 +33,78 @@ from Powers.misc import SUDOERS
 from Powers.utils.pastebin import AnonyBin
 from Powers.utils.clean_file import remove_markdown_and_html
 from Powers.utils.parser import mention_markdown
+from Powers.utils.http_helper import post
 import speedtest
 
+async def take_screenshot(url: str, full: bool = False):
+    url = "https://" + url if not url.startswith("http") else url
+    payload = {
+        "url": url,
+        "width": 1920,
+        "height": 1080,
+        "scale": 1,
+        "format": "jpeg",
+    }
+    if full:
+        payload["full"] = True
+    data = await post(
+        "https://webscreenshot.vercel.app/api",
+        data=payload,
+    )
+    if "image" not in data:
+        return None
+    b = data["image"].replace("data:image/jpeg;base64,", "")
+    file = BytesIO(b64decode(b))
+    file.name = "webss.jpg"
+    return file
+
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+
+@Nikki.on_cmd("webss")
+async def take_ss(_, message: Message):
+    if len(message.command) < 2:
+        return await eor(message, text="ɢɪᴠᴇ ᴀ ᴜʀʟ ᴛᴏ ғᴇᴛᴄʜ sᴄʀᴇᴇɴsʜᴏᴛ.")
+
+    if len(message.command) == 2:
+        url = message.text.split(None, 1)[1]
+        full = False
+    elif len(message.command) == 3:
+        url = message.text.split(None, 2)[1]
+        full = message.text.split(None, 2)[2].lower().strip() in [
+            "yes",
+            "y",
+            "1",
+            "true",
+        ]
+    else:
+        return await eor(message, text="ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ.")
+
+    m = await eor(message, text="ᴄᴀᴘᴛᴜʀɪɴɢ sᴄʀᴇᴇɴsʜᴏᴛ...")
+
+    try:
+        photo = await take_screenshot(url, full)
+        if not photo:
+            return await m.edit("ғᴀɪʟᴇᴅ ᴛᴏ ᴛᴀᴋᴇ sᴄʀᴇᴇɴsʜᴏᴛ.")
+
+        m = await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ...")
+
+        if not full:
+            await message.reply_document(photo)
+        else:
+            await message.reply_document(photo)
+        await m.delete()
+    except Exception as e:
+        await m.edit(str(e))
+# try
 IS_BROADCASTING = False
 
 @Nikki.on_message(command("broadcast") & SUDOERS)
